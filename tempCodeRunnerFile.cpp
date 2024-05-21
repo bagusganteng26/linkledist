3d4ab0d7b379a427a7f73d74799bba0e9337b4b1
BagusEko <sonexb26@gmail.com>
2024-05-22 00:18:48 +0700

    dbl tengah

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,3 @@
-al("nopal", "30 mei 2000", 200000);
-    // tambahGuri("iman", "11 april", 40000);
-    // ubahAwal("giska", "26 februari", 5000);
-    // ubahLast("hudul", "50 agustus", 10000);
-    // hapusAwal();
-    // hapusLast();
-    // cetak();
+string data4[4] = {"den bagus", "denbaguse", "denba@gmail.com", "katasandi"};
+// tambahTengah(data4, 2);
+// cetak();
