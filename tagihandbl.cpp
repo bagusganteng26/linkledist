b440adc13d2ad61a3b94bc05228b1339205b2c80
BagusEko <sonexb26@gmail.com>
2024-05-22 11:29:52 +0700

    edit

--- tagihandbl.cpp
+++ tagihandbl.cpp
@@ -103,6 +103,63 @@ void tambahTengah(string pelanggan, string tanggal, double jumlah, int posisi){
     }
 }
 
+void editAwal (string pelanggan, string tanggal, double jumlah) {
+            if (kepala == NULL)
+    {
+        cout << "Double Linkedlist belum dibuat";
+    }else
+    {
+        kepala ->pelanggan = pelanggan;
+        kepala ->tanggal= tanggal;
+        kepala ->jumlah= jumlah;
+}
+}
+
+void editLast (string pelanggan, string tanggal, double jumlah) {
+                if (kepala == NULL)
+    {
+        cout << "Double Linkedlist belum dibuat";
+    }else
+    {
+        bt ->pelanggan = pelanggan;
+        bt ->tanggal= tanggal;
+        bt ->jumlah= jumlah;
+}
+}
+
+void editTengah(string pelanggan, string tanggal, double jumlah, int posisi) {
+    if (kepala == NULL)
+    {
+        cout << "Double Linkedlist belum dibuat";
+    }else{
+        if (posisi == 1)
+        {
+            cout << "posisi awal bukan posisi tengah" << endl;
+        }else if (posisi < 1 || posisi > hitung())
+        {
+            cout << "posisi ngawur" << endl;
+        }else
+        {
+        nodeBaru = new Tagihan();
+        nodeBaru ->pelanggan = pelanggan;
+        nodeBaru ->tanggal= tanggal;
+        nodeBaru ->jumlah= jumlah;
+
+        //transfersing
+        ini = kepala;
+        int nomor = 1;
+        while (nomor < posisi-1)
+        {
+            ini = ini ->next;
+            nomor++;
+        }
+        ini ->pelanggan = pelanggan;
+        ini ->tanggal= tanggal;
+        ini ->jumlah= jumlah;
+}
+}
+}
+
 void hapusAwal(){
     if (kepala == NULL)
     {
@@ -177,6 +234,8 @@ void cetak() {
 int main () {
 buatDobel("Noval", "2 mei 2023", 20.500);
 tambahAwal("Bagus", "4 maret 2020", 30000);
+editAwal("Gamblok", "15 juni 2000", 35000);
+editLast("Denayot", "12 Desember 2010", 15000);
 cetak();
 // tambahLast("Hilmi", "13 januari", 50000);
 // cetak();
